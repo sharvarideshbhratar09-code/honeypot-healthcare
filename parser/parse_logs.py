fed797cd1293c4f18226b5b14e5eba06cc21192c
sharvarideshbhratar09-code <sharvarideshbhratar09@email.com>
2026-04-08 23:11:21 -0400

    Created initial log parser script for Cowrie logs

--- parser/parse_logs.py
+++ parser/parse_logs.py
@@ -1,25 +1,51 @@
 import json
+from collections import Counter
+import matplotlib.pyplot as plt
 
-log_file = "../logs/cowrie.json"
-
-ips = {}
+ips = []
 commands = []
 
-with open(log_file) as f:
+# Read Cowrie log file
+with open("../logs/cowrie.json") as f:
     for line in f:
-        data = json.loads(line)
+        log = json.loads(line)
 
-        if "src_ip" in data:
-            ip = data["src_ip"]
-            ips[ip] = ips.get(ip, 0) + 1
+        # Extract attacker IPs
+        if "src_ip" in log:
+            ips.append(log["src_ip"])
 
-        if data.get("eventid")  == "cowrie.command.input":
-            commands.append(data["input"])
+        # Extract commands used by attackers
+        if log.get("eventid") == "cowrie.command.input":
+            commands.append(log.get("input"))
 
-print("\nTop Attacker IPs\n")
-for ip, count in ips.items():
+# Show Top Attacker IPs
+print("Top Attacker IPs:")
+top_ips = Counter(ips).most_common(5)
+for ip, count in top_ips:
     print(ip, count)
 
-print("\nCommands Executed by Attackers\n")
-for cmd in commands:
+# Show Commands
+print("\nCommands Executed by Attackers:")
+for cmd in set(commands):
     print(cmd)
+
+# Save report to file
+with open("report.txt", "w") as f:
+    f.write("Top Attacker IPs:\n")
+    for ip, count in top_ips:
+        f.write(f"{ip} {count}\n")
+
+    f.write("\nCommands Executed:\n")
+    for cmd in set(commands):
+        f.write(f"{cmd}\n")
+
+# Create graph
+labels = [ip for ip, _ in top_ips]
+values = [count for _, count in top_ips]
+
+plt.bar(labels, values)
+plt.title("Top Attacker IPs")
+plt.xlabel("IP Address")
+plt.ylabel("Number of Attacks")
+plt.savefig("attacks.png")
+plt.show()
